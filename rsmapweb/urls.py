49296492d12f89a8f060c24523cd1b56e2793a60
José Manuel Luque Burgos <luqueburgosjm@gmail.com>
2016-09-05 21:13:51 +0200

    adding templates

--- rsmapweb/urls.py
+++ rsmapweb/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from rsmapweb import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^api/signals/$', views.SignalList.as_view(), name='signal-list'),
     url(r'^api/signal/(?P<pk>[a-z0-9]+)/$', views.SignalDetail.as_view(), name='signal-detail'),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^map/$', TemplateView.as_view(template_name='map.html')),
 ]
