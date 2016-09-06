5603057e923a51ac625b79695f5e74a4829f517b
José Manuel Luque Burgos <luqueburgosjm@gmail.com>
2016-09-06 17:50:45 +0200

    added suffix_patterns to rest service

--- rsmapweb/urls.py
+++ rsmapweb/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from rsmapweb import views
 from django.views.generic import TemplateView
+from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
+urlpatterns = format_suffix_patterns([
     url(r'^api/signals/$', views.SignalList.as_view(), name='signal-list'),
     url(r'^api/signal/(?P<pk>[a-z0-9]+)/$', views.SignalDetail.as_view(), name='signal-detail'),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^map/$', TemplateView.as_view(template_name='map.html')),
-]
+])
